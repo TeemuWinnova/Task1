bcfd8972f393851bf12dadb9124483f9d444ae43
TeemuWinnova <teemu.winnova@hotmail.com>
2021-11-25 11:41:22 +0200

    first commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int luku1;
+    int luku2;
+    cout << "Sy\x94t\x84 ensimm\x84inen luku." << endl;
+    cin >> luku1;
+    cout << "Sy\x94t\x84 toinen luku." << endl;
+    cin >> luku2;
+    cout << "Lukujen summa on " << luku1 + luku2 << endl;
+    cout << "Lukujen erotus on " << luku1 - luku2 << endl;
     return 0;
 }
